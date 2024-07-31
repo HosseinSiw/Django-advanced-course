ee2b8ca5b69b2ca9041e5b633bf8da278621dedf
Hossein siadati <hosseinsiadati01@gmail.com>
2024-07-31 08:17:56 +0330

    4rd part of rest season 6

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
-
 from blog.models import Post, Category
+from users.models import Profile
+from django.utils.timezone import now
 
 
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(min_length=1, max_length=255)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
 
 class PostSerializer(serializers.ModelSerializer):
     absolute_url = serializers.SerializerMethodField()
-    slug = serializers.ReadOnlyField(source='get_slug')
+    slug = serializers.ReadOnlyField(source='get_slug')  # Model depended fields
     relative_url = serializers.ReadOnlyField(source='get_absolute_api_url')
 
     class Meta:
@@ -22,9 +29,22 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
 
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get("kwargs").get("pk"):
+            rep.pop("relative_url", None)
+            rep.pop("absolute_url", None)
+            rep.pop("slug", None)
+        else:
+            rep.pop("content", None)
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
+        rep['category'] = CategorySerializer(many=True, instance=instance.category).data
+        return rep
+
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id=self.context['request'].user.id)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['updated_time'] = now()
